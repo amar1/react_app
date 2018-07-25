7141bb1c17b105620436919c7a0e288f61a11144
Amrendra Mourya <myself.amrendramourya@gmail.com>
2018-07-25 17:15:50 +0530

    test

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+class App extends React.Component {
+  render() {
+    return (
+       <div>
+          <h1>Header</h1>
+          <h2>Content  Amar</h2>
+          <p>This is the content!!!</p>
+       </div>
+    );
+  }
+}
+export default App;
